a8d508c6128dbc1639be822ddbc73159df3451fc
niccolo_zanieri <niccolo.zanieri.13@gmail.com>
2021-12-28 17:44:44 +0100

    added test folder and User ctor

--- src/main/java/com/example/niccolo_zanieri_swe/model/User.java
+++ src/main/java/com/example/niccolo_zanieri_swe/model/User.java
@@ -1,5 +1,7 @@
 package com.example.niccolo_zanieri_swe.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -8,4 +10,21 @@ enum Tags {
 }
 
 public abstract class User {
+    public User(String usr, String email, String psw, Tags[] tags) {
+        username = usr;
+        this.email = email;
+        password = psw;
+
+        Collections.addAll(favouriteTags, tags);
+        for(Tags tag : Tags.values()) {
+            likesPerTag.put(tag, 0);
+        }
+    }
+
+    private Map<Tags, Integer> likesPerTag = new EnumMap<>(Tags.class);
+    private ArrayList<User> followers = new ArrayList<>();
+    private ArrayList<Tags> favouriteTags = new ArrayList<>();
+    private String username;
+    private String email;
+    private String password;
 }
---
+++ src/test/java/com/example/niccolo_zanieri_swe/model/UserTests.java
@@ -0,0 +1,6 @@
+package com.example.niccolo_zanieri_swe.model;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTests {
+}
