6b8dfca84f427f4a4d26fc48f72884150cf16742
niccolo_zanieri <niccolo.zanieri.13@gmail.com>
2021-12-28 17:56:06 +0100

    changed followers to followed and added follow method

--- src/main/java/com/example/niccolo_zanieri_swe/model/User.java
+++ src/main/java/com/example/niccolo_zanieri_swe/model/User.java
@@ -1,9 +1,6 @@
 package com.example.niccolo_zanieri_swe.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.EnumMap;
-import java.util.Map;
+import java.util.*;
 
 enum Tags {
     LANDSCAPE, PORTRAIT, STREET, ACTION, ADVENTURE, COMEDY, THRILLER, VISUAL_IDENTITY, ADVERTISING, UI, MOTION_GRAPHIC
@@ -21,8 +18,65 @@ public abstract class User {
         }
     }
 
+    public boolean isFollowed(User u) {
+         return followed.containsKey(u.username);
+    }
+
+    public void follow(User u) {
+        if(this.isFollowed(u))
+            throw new IllegalArgumentException("Selected user is already followed.");
+    }
+    
+    public Map<Tags, Integer> getLikesPerTag() {
+        return likesPerTag;
+    }
+
+    public void setLikesPerTag(Map<Tags, Integer> likesPerTag) {
+        this.likesPerTag = likesPerTag;
+    }
+
+    public Hashtable<String, User> getFollowed() {
+        return followed;
+    }
+
+    public void setFollowed(Hashtable<String, User> followed) {
+        this.followed = followed;
+    }
+
+    public ArrayList<Tags> getFavouriteTags() {
+        return favouriteTags;
+    }
+
+    public void setFavouriteTags(ArrayList<Tags> favouriteTags) {
+        this.favouriteTags = favouriteTags;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private Map<Tags, Integer> likesPerTag = new EnumMap<>(Tags.class);
-    private ArrayList<User> followers = new ArrayList<>();
+    private Hashtable<String, User> followed = new Hashtable<>();
     private ArrayList<Tags> favouriteTags = new ArrayList<>();
     private String username;
     private String email;
