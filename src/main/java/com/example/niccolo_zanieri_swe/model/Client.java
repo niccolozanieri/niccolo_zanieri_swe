b101e3cb591868e8537ad66c3a18a8fd1c9d1480
niccolo_zanieri <niccolo.zanieri.13@gmail.com>
2022-01-01 17:24:23 +0100

    added basic Client implementation

--- src/main/java/com/example/niccolo_zanieri_swe/model/Client.java
+++ src/main/java/com/example/niccolo_zanieri_swe/model/Client.java
@@ -1,4 +1,13 @@
 package com.example.niccolo_zanieri_swe.model;
 
-public class Client {
+import java.util.Collections;
+
+public class Client extends User {
+    public Client(String usr, String email, String psw) {
+        super(usr, email, psw);
+    }
+
+    public Client(String usr, String email, String psw, Tags[] tags) {
+        super(usr, email, psw, tags);
+    }
 }
