6d004d611fbeb67a7e7e9458b5bff8c53d842194
niccolo_zanieri <niccolo.zanieri.13@gmail.com>
2022-01-22 10:21:24 +0100

    added values to creator/client choice box

--- src/main/java/com/niccolo_zanieri_swe/SessionController.java
+++ src/main/java/com/niccolo_zanieri_swe/SessionController.java
@@ -2,20 +2,35 @@ package com.niccolo_zanieri_swe;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class SessionController {
+public class SessionController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> creatorClientChB = new ChoiceBox<>();
+    private String[] userTypes = {"Creator", "Client"};
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if(creatorClientChB.getItems().isEmpty()) {
+            creatorClientChB.getItems().addAll(userTypes);
+        }
+    }
 
     @FXML
     public void switchToSignUp(MouseEvent event) throws IOException {
