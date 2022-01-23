a5d867ba977ac1c2ddf2293a8e4cf05f3511913b
niccolo_zanieri <niccolo.zanieri.13@gmail.com>
2022-01-23 17:10:02 +0100

    added admin controllers for Creator and Client

---
+++ src/main/java/com/niccolo_zanieri_swe/ClientAdminController.java
@@ -0,0 +1,33 @@
+package com.niccolo_zanieri_swe;
+
+import com.niccolo_zanieri_swe.dao.ClientDAO;
+import com.niccolo_zanieri_swe.dao.ConnectionPool;
+import com.niccolo_zanieri_swe.dao.CreatorDAO;
+import javafx.fxml.FXML;
+
+import java.sql.SQLException;
+
+public class ClientAdminController {
+
+    public ClientAdminController() {
+        String url = "jdbc:postgresql://localhost:5432/swe_exam_db";
+        String usr = "niccolo_zanieri";
+        String psw = "basidati2022";
+        try {
+            this.dao = new ClientDAO(ConnectionPool.getConnectionPool(url, usr, psw));
+        } catch(SQLException e) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        }
+    }
+
+    @FXML
+    public void registerClient(String usr, String email, String psw) {
+        try {
+            this.dao.insertClient(usr, email, psw);
+        } catch(SQLException e) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        }
+    }
+
+    private ClientDAO dao;
+}
---
+++ src/main/java/com/niccolo_zanieri_swe/CreatorAdminController.java
@@ -0,0 +1,31 @@
+package com.niccolo_zanieri_swe;
+
+import com.niccolo_zanieri_swe.dao.ConnectionPool;
+import com.niccolo_zanieri_swe.dao.CreatorDAO;
+import javafx.fxml.FXML;
+
+import java.sql.SQLException;
+
+public class CreatorAdminController {    // TODO: create pages to show errors
+    public CreatorAdminController() {
+        String url = "jdbc:postgresql://localhost:5432/swe_exam_db";
+        String usr = "niccolo_zanieri";
+        String psw = "basidati2022";
+        try {
+            this.dao = new CreatorDAO(ConnectionPool.getConnectionPool(url, usr, psw));
+        } catch(SQLException e) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        }
+    }
+    
+    @FXML
+    public void registerCreator(String usr, String email, String psw) {
+        try {
+            this.dao.insertCreator(usr, email, psw);
+        } catch(SQLException e) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+        }
+    }
+
+    private CreatorDAO dao;
+}
